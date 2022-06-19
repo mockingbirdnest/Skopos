d5f6edc8ffbf90879e1e48fe8f4785cf1a847f5d
Robin Leroy <egg.robin.leroy@gmail.com>
2022-06-19 18:22:31 +0200

    Fix sustained failing intermittent maintenance

---
+++ Telecom/ContractNotCompletedOnAcceptance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContractConfigurator;
+
+namespace σκοπός {
+
+  // Same as CompleteContract, except it is inverted, and it does not checkOnActiveContract.
+  public class ContractNotCompletedOnAcceptance : CompleteContractRequirement  {
+    public ContractNotCompletedOnAcceptance() {}
+    public override bool LoadFromConfig(ConfigNode configNode) {
+      bool valid = base.LoadFromConfig(configNode);
+      checkOnActiveContract = false;
+      invertRequirement = true;
+      return valid;
+    }
+  }
+
+}
