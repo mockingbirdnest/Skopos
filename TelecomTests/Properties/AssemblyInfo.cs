1b1348e10287151e5eafb28ea2621214dc9d2da3
Robin Leroy <egg.robin.leroy@gmail.com>
2022-07-03 19:48:59 +0200

    Now I need to write tests

---
+++ TelecomTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TelecomTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TelecomTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3109dff2-12c5-4daf-8196-6094dd91b4b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TelecomTests/routing_test.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace σκοπός {
+[TestClass]
+public class RoutingTest {
+  [TestMethod]
+  public void Meow() {
+  }
+}
+}
