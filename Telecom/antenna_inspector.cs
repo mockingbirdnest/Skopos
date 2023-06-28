9bab0446510ab6eb4d33240b0f1decb93a480cf8
Robin Leroy <egg.robin.leroy@gmail.com>
2023-06-28 02:05:00 +0200

    spectrum usage inspection

--- Telecom/antenna_inspector.cs
+++ Telecom/antenna_inspector.cs
@@ -50,6 +50,52 @@ internal class AntennaInspector : principia.ksp_plugin_adapter.SupervisedWindowR
 
       }
     }
+    Style.HorizontalLine();
+    UnityEngine.GUILayout.Label("Spectrum usage:");
+    foreach (var broadcast in spectrum.usages) {
+      string pretty_bandwidth = $"{RATools.PrettyPrint(broadcast[0].spectrum)}Hz";
+      if (broadcast[0].kind == Routing.NetworkUsage.SpectrumBreakdown.SingleUsage.Kind.Transmit) {
+        if (broadcast.Length == 1) {
+          UnityEngine.GUILayout.Label(
+              $@"{pretty_bandwidth} transmitting to {broadcast[0].link.link.rx.displayName} for {
+              RATools.PrettyPrintDataRate(broadcast[0].link.connection.data_rate)} {
+              broadcast[0].link.channel.links[0].tx.displayName}–{
+              broadcast[0].link.channel.links.Last().rx.displayName}");
+        } else {
+          UnityEngine.GUILayout.Label(
+              $@"{pretty_bandwidth} transmitting to {string.Join(
+                  ", ",
+                  broadcast.Select(
+                      usage => usage.link.link.rx.displayName).Distinct())} for {
+                      RATools.PrettyPrintDataRate(
+                          broadcast[0].link.connection.data_rate)} broadcast from {
+              broadcast[0].link.channel.links[0].tx.displayName} to {string.Join(
+                  ", ",
+                broadcast.Select(
+                        usage => usage.link.channel.links.Last().rx.displayName))}");
+
+        }
+      } else {
+        if (broadcast.Length == 1) {
+          UnityEngine.GUILayout.Label(
+              $@"{pretty_bandwidth} receiving from {broadcast[0].link.link.tx.displayName} for {
+              RATools.PrettyPrintDataRate(broadcast[0].link.connection.data_rate)} {
+              broadcast[0].link.channel.links[0].tx.displayName}–{
+              broadcast[0].link.channel.links.Last().rx.displayName}");
+        } else {
+          UnityEngine.GUILayout.Label(
+              $@"{pretty_bandwidth} receiving from {broadcast[0].link.link.tx.displayName} for {
+                      RATools.PrettyPrintDataRate(
+                          broadcast[0].link.connection.data_rate)} broadcast from {
+              broadcast[0].link.channel.links[0].tx.displayName} to {string.Join(
+                  ", ",
+                broadcast.Select(
+                        usage => usage.link.channel.links.Last().rx.displayName))}");
+
+        }
+      }
+    }
+    Style.HorizontalLine();
     UnityEngine.GUI.DragWindow();
   }
 
